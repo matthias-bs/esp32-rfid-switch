e1ec8f9354f7f6d263cab65a78cfce8c6baa106f
Matthias Prinke <m.prinke@arcor.de>
2026-09-09 21:17:11 +0200

    Update header comments in rfid-tag-write.ino

--- examples/rfid-tag-write/rfid-tag-write.ino
+++ examples/rfid-tag-write/rfid-tag-write.ino
@@ -1,3 +1,49 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// rfid-tag-write.ino
+//
+// Example for RFID Tag Switch - Tag Writer
+//
+// Identify RFID tags and initialize their EPC/TID configuration, optional access password,
+// and optional User Memory token from a JSON object received through the serial interface.
+// User Memory is locked after initialization to match the runtime switch examples.
+//
+// https://github.com/matthias-bs/esp32-rfid-switch
+//
+//
+// created: 09/2026
+//
+//
+// MIT License
+//
+// Copyright (c) 2026 Matthias Prinke
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// History:
+//
+// 20260909 Initial
+//
+// ToDo:
+// -
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #include <UNIT_UHF_RFID.h>
